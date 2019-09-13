3772d1a7d498e7ad847b522931f3d229a64ba735
Ben Lapuhapo <ben.lapuhapo@mths.ca>
2019-09-13 16:33:03 +0000

    Added circle calculator

---
+++ circle_calculator.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+# Created by: Ben Lapuhapo
+# Created on: September 12 2019
+# This program calculates area and perimeter of a circle
+
+
+import math
+
+
+def main():
+    print("The circle's Dimensions is R=15mm")
+    print("The circle's  Circumference is {}".format(math.pi*15*2))
+    print("The circle's  Area is {}".format(2*math.pi*15))
+
+
+
+if __name__ == "__main__":
+    main()
